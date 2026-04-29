61eadb5dd4f99e0fd3dc2b94fa484dd87d5c61cc
Ritvars <ritvarsbaders@gmail.com>
2026-04-29 09:21:53 +0300

    Init

---
+++ src/main/java/lv/venta/JavaSerminars6Application.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSerminars6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSerminars6Application.class, args);
+	}
+
+}
