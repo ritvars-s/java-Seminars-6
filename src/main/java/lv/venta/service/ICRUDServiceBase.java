1a8b76e5e06c5938caa2f08c130b3c332313e0ed
Ritvars <ritvarsbaders@gmail.com>
2026-05-06 09:47:57 +0300

    Added: service and creates ICRUDServiceBase

--- src/main/java/lv/venta/service/ICRUDServiceBase.java
+++ src/main/java/lv/venta/service/ICRUDServiceBase.java
@@ -1,5 +1,17 @@
 package lv.venta.service;
 
-public interface ICRUDServiceBase {
+import java.util.ArrayList;
 
+
+//Ttpe template datutips kas pielagosies velak jau konkretaja CRUD interface
+public interface ICRUDServiceBase<Ttype> {
+	//CRUD -ka baze tam funkcijam kuras var sablonizet - retrieveAll , retrieve by id delete by id
+	public abstract 
+		//R -retrieve all
+	public abstract ArrayList<Ttype> retriveAll() throws Excetion;
+	// R retrieve by id
+	public abstract Ttype retrieveByID(long id) throws Exception;
+	//D - delete by id
+	public abstract void deleteByID(long id) throws Exception;
+	
 }
